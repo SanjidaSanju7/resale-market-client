a7eb525c0d8e0884088c40908b913d2871da1d4e
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-11-25 22:57:11 +0600

    signup page created

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,93 +1,65 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleSignUp = data => {
+        console.log(data);
+    }
+
     return (
         <div className="bg-white rounded shadow-2xl p-7 w-full lg:w-1/3 mx-auto text-left mt-8 mb-5">
             <h3 className="mb-4 text-xl font-semibold sm:mb-6 sm:text-2xl">
                 Please Register
             </h3>
-            <form >
-                <div className="mb-1 sm:mb-2">
-                    <label
-                        htmlFor="email"
-                        className="inline-block mb-1 font-medium"
-                    >
-                        Email
-                    </label>
-                    <input
-                        placeholder="Your Email"
-                        required
-                        type="email"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="email"
-                        name="email"
-                    />
-                </div>
-                <div className="mb-1 sm:mb-2">
-                    <label
-                        htmlFor="password"
-                        className="inline-block mb-1 font-medium"
-                    >
-                        Password
-                    </label>
-                    <input
-                        placeholder="Your Password"
-                        required
-                        type="password"
-                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                        id="password"
-                        name="password"
-                    />
-                </div>
-                <label
-                    htmlFor=""
-                    className="inline-block mb-1 font-medium"
-                >
-                    Choose an option
-                </label>
-                <div>
-                    <select className="select select-bordered w-full">
-                        <option disabled selected>Seller</option>
-                        <option>Seller</option>
-                        <option>Buyer</option>
-                    </select>
-                </div>
+            <div className='mt-14 flex justify-center items-center '>
+                <div className='w-96 p-8' >
+                    <h2 >Please Login</h2>
+                    <form onSubmit={handleSubmit(handleSignUp)}>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label"> <span className="label-text">Name</span></label>
+                            <input type="text" {...register("name", {
+                                required: "Name is Required"
+                            })} className="input input-bordered w-full max-w-xs" />
+                            {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label"><span className="label-text">Email</span></label>
+                            <input type="email" {...register("email", {
+                                required: "Email Address is required"
+                            })} className="input input-bordered w-full max-w-xs" />
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label"><span className="label-text">Password</span></label>
+                            <input type="password" {...register("password", {
+                                required: "Password id required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            })} className="input input-bordered w-full max-w-xs" />
+                            {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label"><span className="label-text">Select an Option</span></label>
+                            <select type='text' {...register("category", { required: true })} className="input input-bordered w-full max-w-xs" >
+                                <option value="">Select...</option>
+                                <option value="Seller">Seller</option>
+                                <option value="Buyer">Buyer</option>
+                            </select>
+                        </div>
 
-                <div className="mt-4 mb-2 sm:mb-4">
-                    <p className="text-xs text-red-500 sm:text-sm text-center">
+                        <input value="Sign Up" type="submit" className='btn btn-outline w-full mt-4' />
+                    </form>
+                    <p>Already have an account?<Link className='text-blue-500' to="/login">Create new Account</Link></p>
 
-                    </p>
                 </div>
-                <div className="mt-4 mb-2 sm:mb-4">
-                    <button
-                        type="submit"
-                        className="btn btn-sm btn-outline w-full"
-                    >
-                        Login
-                    </button>
-                </div>
-                <div className="mt-4 mb-4 sm:mb-4">
-                    <p className="text-xs text-center text-gray-600 sm:text-sm font-medium ">
-                        Social Login
-                    </p>
-                    <button
-
-                        type="submit"
-                        className="btn btn-sm btn-outline w-full mt-4"
-                    >
-                        <p className='ml-2'>Log In with Google</p>
-                    </button>
-                </div>
-
-                <p className="text-xs text-gray-600 sm:text-sm text-center">
-                    Already have an account?
-                    <Link to='/login' className="text-purple-600"> Sign Up.</Link>
-                </p>
-            </form>
+            </div>
         </div>
 
     );
 };
 
 export default SignUp;
+
